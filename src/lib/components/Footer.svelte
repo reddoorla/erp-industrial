546b342b8f6899e9d7a09bc061c639d84f7bc1be
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-04-30 11:54:12 -0700

    d

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -38,8 +38,8 @@ text-transform: uppercase;
 
 <svelte:window bind:innerWidth={viewportWidth} />
 
-<div class="bg-black w-screen sticky snap-end h-screen top-0 md:h-[30vh] md:top-[70vh]">
-    <ContentWidth class="flex flex-row pt-6 h-full">
+<div class="bg-black w-screen sticky snap-end h-screen top-0 md:h-[20vh] md:top-[80vh]">
+    <ContentWidth class="flex flex-row pt-6 h-full" >
         {#if viewportWidth>768}
         <div class="h-full w-1/3 relative">
             <a href="/" class="w-[25%] hover:opacity-80 transition-all duration-500 bump brightness-0 invert relative block">
