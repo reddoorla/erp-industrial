ce0d4a58f4bb92fba704afae0c8d8bddcfb76d06
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-05-15 17:59:38 -0700

    bumped opacity

--- src/lib/components/FullPageSlide.svelte
+++ src/lib/components/FullPageSlide.svelte
@@ -15,7 +15,7 @@
 <div class="w-screen h-screen overflow-hidden">
     {#if backgroundImage}
         <PrismicImage field={backgroundImage} class="object-cover absolute {viewportHeight*16 >viewportWidth*9 ? "h-full":"w-full"}"/>
-        <div class="w-full h-full absolute top-0 left-0" style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 14.68%, rgba(0, 0, 0, 0.30) 69.9%)"></div>
+        <div class="w-full h-full absolute top-0 left-0" style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 14.68%, rgba(0, 0, 0, 0.50) 69.9%)"></div>
     {/if}
 
     <slot />
