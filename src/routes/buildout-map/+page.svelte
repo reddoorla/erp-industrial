5fdacb155d7fa3ec1dcbd5185dd932f05b53faac
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-06-13 09:59:15 -0700

    link fixes

--- src/routes/buildout-map/+page.svelte
+++ src/routes/buildout-map/+page.svelte
@@ -1,25 +1,23 @@
 <script lang="ts">
 	import Nav from "$lib/components/Nav.svelte";
+	import { onMount } from "svelte";
 
-const buildoutHTML = `
-  <div id="buildout"  style="max-width:1440px; margin:auto; height:80vh;"></div>
-  <script type="text/javascript" src="//buildout.com/api.js?v8"><\/script>
-  <script type="text/javascript">
-    BuildOut.embed({
-      token: "bdecc802689ae7f3e2007fdaf2ffdb31f711a99e",
-      plugin: "inventory",
-      target: "buildout"
-    });
-  <\/script>
-`;
+
+onMount(()=>{
+  //@ts-ignore
+  BuildOut.embed({    token:     "bdecc802689ae7f3e2007fdaf2ffdb31f711a99e",  plugin:    "inventory",
+				target:    "buildout"
+			});
+})
 </script>
 
 <svelte:head>
     <title>ERP Properties</title>
 </svelte:head>
+
 <div class="w-screen h-screen bg-white fixed top-0"> 
     <Nav isLogoLarge={false}/>
 <div class="mt-[20vh]">
-    {@html buildoutHTML}
+  <div id="buildout"  style="max-width:1440px; margin:auto; height:80vh;"></div>
 </div>
 </div>  
