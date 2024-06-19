6520d17535d53e907b92de50ca9cd824f0457feb
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-06-18 22:30:03 -0700

    email changed back

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -29,7 +29,7 @@ export const actions: Actions = {
 
     switch(interest){
       case "Leasing":
-        sendTo = ['Brennan Berry <tucker@reddoorla.com>'];
+        sendTo = ['Brennan Berry <BBerry@erpfunds.com>'];
         break;
       
       case "Investor Relations":
