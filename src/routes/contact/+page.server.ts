25e9e064ca5c5b1d2442de25fa668f303cc13168
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-06-18 22:10:26 -0700

    testing

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -29,7 +29,7 @@ export const actions: Actions = {
 
     switch(interest){
       case "Leasing":
-        sendTo = ['Brennan Berry <BBerry@erpfunds.com>'];
+        sendTo = ['Brennan Berry <tucker@reddoorla.com>'];
         break;
       
       case "Investor Relations":
