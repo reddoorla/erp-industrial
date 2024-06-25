d5a356afa8fc9120bbf022273f2e9143eb36e196
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-06-25 15:25:50 -0700

    py-32

--- src/lib/slices/Hero/index.svelte
+++ src/lib/slices/Hero/index.svelte
@@ -107,7 +107,7 @@ const handleMouseMove = (event: MouseEvent) => {
 
 
 	<ContentWidth class="h-full relative justify-end z-30">
-		<div class="md:w-11/12 max-w-screen-lg relative h-full flex flex-col md:justify-end overflow-y-auto md:overflow-hidden pb-32 md:pb-0">
+		<div class="md:w-11/12 max-w-screen-lg relative h-full flex flex-col md:justify-end overflow-y-auto md:overflow-hidden py-32 md:pb-0">
 		{#if isMounted}
 		<div transition:fade>
 		<ContentBox 
